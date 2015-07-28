sbcl/sbcl

commit 7635bc92ae31bbf1a8b2860e7562ede298952354

Optimize out of line MAP1.
It's rarely used out of line, but some simple modifications allow it
to be faster and cons less.

src/code/list.lisp
@@ -1237,26 +1237,28 @@
 ;;; way. It is done when any of the arglists runs out. Until then, it
 ;;; CDRs down the arglists calling the function and accumulating
 ;;; results as desired.
-(defun map1 (fun-designator original-arglists accumulate take-car)
-  (let ((fun (%coerce-callable-to-fun fun-designator)))
-    (let* ((arglists (copy-list original-arglists))
-           (ret-list (list nil))
-           (temp ret-list))
-      (do ((res nil)
-           (args '() '()))
-          ((dolist (x arglists nil) (if (null x) (return t)))
-           (if accumulate
-               (cdr ret-list)
-               (car original-arglists)))
-        (do ((l arglists (cdr l)))
-            ((null l))
-          (push (if take-car (caar l) (car l)) args)
-          (setf (car l) (cdar l)))
-        (setq res (apply fun (nreverse args)))
-        (case accumulate
-          (:nconc (setq temp (last (nconc temp res))))
-          (:list (rplacd temp (list res))
-                 (setq temp (cdr temp))))))))
+(defun map1 (fun-designator arglists accumulate take-car)
+  (do* ((fun (%coerce-callable-to-fun fun-designator))
+        (non-acc-result (car arglists))
+        (ret-list (list nil))
+        (temp ret-list)
+        (res nil)
+        (args (make-list (length arglists))))
+       ((dolist (x arglists) (or x (return t)))
+        (if accumulate
+            (cdr ret-list)
+            non-acc-result))
+    (do ((l arglists (cdr l))
+         (arg args (cdr args)))
+        ((null l))
+      (setf (car arg) (if take-car (caar l) (car l)))
+      (setf (car l) (cdar l)))
+    (setq res (apply fun args))
+    (case accumulate
+      (:nconc (setf (cdr temp) res
+                    temp (last res)))
+      (:list (setf (cdr temp) (list res)
+                   temp (cdr temp))))))
 
 (defun mapc (function list &rest more-lists)
   #!+sb-doc

richardwesthaver/cl

commit 40cd776ea5c61f7477b7b1b80667f241915ed17b

Fix MAP1 (again)
Iterate through arguments correctly for more than two lists.

This fixes a (never released) regression introduced in commit
7635bc92ae31bbf1a8b2860e7562ede298952354.

Reported-by: Fare in #sbcl

src/code/list.lisp
@@ -1249,7 +1249,7 @@
             (cdr ret-list)
             non-acc-result))
     (do ((l arglists (cdr l))
-         (arg args (cdr args)))
+         (arg args (cdr arg)))
         ((null l))
       (setf (car arg) (if take-car (caar l) (car l)))
       (setf (car l) (cdar l)))

tests/map-tests.impure.lisp
@@ -244,4 +244,6 @@
                    '((1 2 3) (3 2 1) (2 3) (2 1) (3) (1))))
     (assert (equal (call 'mapcan #'identity
                          '((3 4 . 5) nil (1 . 5)))
-                   '(3 4 1 . 5)))))
+                   '(3 4 1 . 5)))
+    (assert (equal (call 'mapcar #'list '(1 2 3) '(4 5 6) '(7 8 9))
+                   '((1 4 7) (2 5 8) (3 6 9))))))