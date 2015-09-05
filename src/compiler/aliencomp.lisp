sbcl/sbcl

commit e0721f45d2a1b750f624b09e070f4a4bd6aa9419

Add ENSURE-LIST, use where appropriate

src/compiler/aliencomp.lisp
@@ -812,28 +812,26 @@
                   (vop sb!vm::move-single-to-int-arg call block
                        float-tn i1-tn))))))
       (aver (null args))
-      (unless (listp result-tns)
-        (setf result-tns (list result-tns)))
       (let ((arg-tns (remove-if-not #'tn-p (flatten-list arg-tns)))
-            (result-tns (remove-if-not #'tn-p result-tns)))
+            (result-tns (remove-if-not #'tn-p (ensure-list result-tns))))
         (vop* call-out call block
               ((lvar-tn call block function)
                (reference-tn-list arg-tns nil))
-              ((reference-tn-list result-tns t))))
-      #!-x86
-      (vop dealloc-number-stack-space call block stack-frame-size)
-      #!+x86
-      (progn
-        (vop reset-stack-pointer call block stack-pointer)
-        (vop set-fpu-word-for-lisp call block))
-      (cond
-        #!+arm-softfp
-        ((and lvar
-              (proper-list-of-length-p result-tns 3)
-              (symbolp (third result-tns)))
-         (emit-template call block
-                        (template-or-lose (third result-tns))
-                        (reference-tn-list (butlast result-tns) nil)
-                        (reference-tn (car (ir2-lvar-locs (lvar-info lvar))) t)))
-        (t
-         (move-lvar-result call block result-tns lvar))))))
+              ((reference-tn-list result-tns t)))
+        #!-x86
+        (vop dealloc-number-stack-space call block stack-frame-size)
+        #!+x86
+        (progn
+          (vop reset-stack-pointer call block stack-pointer)
+          (vop set-fpu-word-for-lisp call block))
+        (cond
+          #!+arm-softfp
+          ((and lvar
+                (proper-list-of-length-p result-tns 3)
+                (symbolp (third result-tns)))
+           (emit-template call block
+                          (template-or-lose (third result-tns))
+                          (reference-tn-list (butlast result-tns) nil)
+                          (reference-tn (car (ir2-lvar-locs (lvar-info lvar))) t)))
+          (t
+           (move-lvar-result call block result-tns lvar)))))))

contrib/sb-executable/sb-executable.lisp
@@ -48,7 +48,7 @@ exec sbcl --noinform ~{~A ~}--eval \"(with-open-file (i \\\"$0\\\" :element-type
     (write-sequence (map 'vector #'char-code
                          (format nil *exec-header* runtime-flags
                                  (or initial-function 'values))) out)
-    (dolist (input-file (if (listp fasls) fasls (list fasls)))
+    (dolist (input-file (sb-int:ensure-list fasls))
       (with-open-file (in (merge-pathnames input-file
                                            (make-pathname :type "fasl"))
                           :element-type '(unsigned-byte 8))

contrib/sb-introspect/introspect.lisp
@@ -221,11 +221,7 @@ name. Type can currently be one of the following:
 
 If an unsupported TYPE is requested, the function will return NIL.
 "
-  (flet ((listify (x)
-           (if (listp x)
-               x
-               (list x)))
-         (get-class (name)
+  (flet ((get-class (name)
            (and (symbolp name)
                 (find-class name nil)))
          (real-fdefinition (name)
@@ -235,7 +231,7 @@ If an unsupported TYPE is requested, the function will return NIL.
              (if profile-info
                  (sb-profile::profile-info-encapsulated-fun profile-info)
                  (fdefinition name)))))
-    (listify
+    (sb-int:ensure-list
      (case type
        ((:variable)
         (when (and (symbolp name)

package-data-list.lisp-expr
@@ -1238,8 +1238,8 @@ possibly temporarily, because it might be used internally."
                "PROPER-LIST-OF-LENGTH-P" "PROPER-LIST-P"
                "LIST-OF-LENGTH-AT-LEAST-P"
                "LIST-WITH-LENGTH-P"
-               "SINGLETON-P"
-              "MISSING-ARG"
+               "SINGLETON-P" "ENSURE-LIST"
+               "MISSING-ARG"
                "FEATUREP"
                "FLUSH-STANDARD-OUTPUT-STREAMS"
                "WITH-UNIQUE-NAMES" "MAKE-GENSYM-LIST"

src/code/array.lisp
@@ -1109,7 +1109,7 @@ of specialized arrays is supported."
   "Adjust ARRAY's dimensions to the given DIMENSIONS and stuff."
   (when (invalid-array-p array)
     (invalid-array-error array))
-  (binding* ((dimensions (if (listp dimensions) dimensions (list dimensions)))
+  (binding* ((dimensions (ensure-list dimensions))
              (array-rank (array-rank array))
              (()
               (unless (= (length dimensions) array-rank)

src/code/dyncount.lisp
@@ -299,7 +299,7 @@ comments from CMU CL:
 (defun matches-pattern (name pattern)
   (declare (simple-string name))
   (let ((name (concatenate 'string "$" name "$")))
-    (dolist (pat (if (listp pattern) pattern (list pattern)) nil)
+    (dolist (pat (ensure-list pattern) nil)
       (when (search (the simple-string (string pat))
                     name :test #'char=)
         (return t)))))

src/code/early-extensions.lisp
@@ -215,6 +215,10 @@
       (and (consp x)
            (list-of-length-at-least-p (cdr x) (1- n)))))
 
+(declaim (inline ensure-list))
+(defun ensure-list (thing)
+  (if (listp thing) thing (list thing)))
+
 ;;; Is X is a positive prime integer?
 (defun positive-primep (x)
   ;; This happens to be called only from one place in sbcl-0.7.0, and

src/code/loop.lisp
@@ -1464,16 +1464,13 @@ code to be loaded.
 (defun add-loop-path (names function universe
                       &key preposition-groups inclusive-permitted user-data)
   (declare (type loop-universe universe))
-  (unless (listp names)
-    (setq names (list names)))
-  (let ((ht (loop-universe-path-keywords universe))
-        (lp (make-loop-path
+  (let* ((names (sb!int:ensure-list names))
+         (ht (loop-universe-path-keywords universe))
+         (lp (make-loop-path
               :names (mapcar #'symbol-name names)
               :function function
               :user-data user-data
-              :preposition-groups (mapcar (lambda (x)
-                                            (if (listp x) x (list x)))
-                                          preposition-groups)
+              :preposition-groups (mapcar #'sb!int:ensure-list preposition-groups)
               :inclusive-permitted inclusive-permitted)))
     (dolist (name names)
       (setf (gethash (symbol-name name) ht) lp))

richardwesthaver/cl

commit 83fea2673f68693e8318725e5d6079df4aee62c8

Restore returning doubles from alien calls on arm-softfp.
Broken since e0721f45d2a1b750f624b09e070f4a4bd6aa9419.

src/compiler/aliencomp.lisp
@@ -817,12 +817,11 @@
                   (vop sb!vm::move-single-to-int-arg call block
                        float-tn i1-tn))))))
       (aver (null args))
-      (let ((arg-tns (remove-if-not #'tn-p (flatten-list arg-tns)))
-            (result-tns (remove-if-not #'tn-p (ensure-list result-tns))))
+      (let ((result-tns (ensure-list result-tns)))
         (vop* call-out call block
               ((lvar-tn call block function)
-               (reference-tn-list arg-tns nil))
-              ((reference-tn-list result-tns t)))
+               (reference-tn-list (remove-if-not #'tn-p (flatten-list arg-tns)) nil))
+              ((reference-tn-list (remove-if-not #'tn-p result-tns) t)))
         #!-c-stack-is-control-stack
         (vop dealloc-number-stack-space call block stack-frame-size)
         #!+c-stack-is-control-stack