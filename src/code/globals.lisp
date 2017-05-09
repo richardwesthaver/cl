sbcl/sbcl

commit 8ede17a6b75128950ba83bb5e59e6c1b4d6f2746

Safepoint protocol upgrade
Upgrade to the new stop-the-world protocol from the Windows branch,
dating back to late 2011, an all-in-one readability and correctness
improvement.

Replace per-thread qrl locks with one process-wide lock for all
operations relating to GC state and the threads' CSP area.  Use
condition variables for notifications on GC state changes.

Thanks to Anton Kovalenko.

src/compiler/generic/parms.lisp
@@ -157,7 +157,7 @@
     sb!unix::*interrupt-pending*
     #!+sb-thruption sb!unix::*thruption-pending*
     #!+sb-thruption sb!impl::*restart-clusters*
-    *in-without-gcing*
+    sb!vm::*in-without-gcing*
     *gc-inhibit*
     *gc-pending*
     #!-sb-thread

src/runtime/interrupt.c
@@ -944,7 +944,7 @@ interrupt_handle_pending(os_context_t *context)
             sigset_t old, *ctxset = os_context_sigmask_addr(context);
             unblock_signals(&deferrable_sigset, ctxset, &old);
 #endif
-            thread_pitstop(context);
+            thread_in_lisp_raised(context);
 #ifndef LISP_FEATURE_WIN32
             sigcopyset(&old, ctxset);
 #endif

src/runtime/runtime.h
@@ -45,9 +45,25 @@ void os_preinit();
 #endif
 
 #if defined(LISP_FEATURE_SB_SAFEPOINT)
+
+typedef enum {
+    GC_NONE=0,
+    GC_FLIGHT,
+    GC_MESSAGE,
+    GC_INVOKED,
+    GC_QUIET,
+    GC_SETTLED,
+    GC_COLLECT,
+    GC_NPHASES
+}  gc_phase_t;
+
 void map_gc_page();
 void unmap_gc_page();
 int check_pending_interrupts();
+void gc_state_lock();
+void gc_state_wait(gc_phase_t);
+void gc_state_unlock();
+
 #endif
 
 /*

src/runtime/thread.c
@@ -120,6 +120,7 @@ unlink_thread(struct thread *th)
         th->next->prev = th->prev;
 }
 
+#ifndef LISP_FEATURE_SB_SAFEPOINT
 /* Only access thread state with blockables blocked. */
 lispobj
 thread_state(struct thread *thread)
@@ -193,7 +194,8 @@ wait_for_thread_state_change(struct thread *thread, lispobj state)
     }
     thread_sigmask(SIG_SETMASK, &old, NULL);
 }
-#endif
+#endif /* sb-safepoint */
+#endif /* sb-thread */
 
 static int
 initial_thread_trampoline(struct thread *th)
@@ -212,9 +214,6 @@ initial_thread_trampoline(struct thread *th)
     function = th->no_tls_value_marker;
     th->no_tls_value_marker = NO_TLS_VALUE_MARKER_WIDETAG;
     if(arch_os_thread_init(th)==0) return 1;
-#ifdef LISP_FEATURE_SB_SAFEPOINT
-    pthread_mutex_lock(thread_qrl(th));
-#endif
     link_thread(th);
     th->os_thread=thread_self();
 #ifndef LISP_FEATURE_WIN32
@@ -380,7 +379,6 @@ new_thread_trampoline(struct thread *th)
      * it is not). */
 #ifdef LISP_FEATURE_SB_SAFEPOINT
     *th->csp_around_foreign_call = (lispobj)&function;
-    pthread_mutex_lock(thread_qrl(th));
 #endif
     lock_ret = pthread_mutex_lock(&all_threads_lock);
     gc_assert(lock_ret == 0);
@@ -392,6 +390,9 @@ new_thread_trampoline(struct thread *th)
      * non-safepoint versions of this code.  Can we unify this more?
      */
 #ifdef LISP_FEATURE_SB_SAFEPOINT
+    gc_state_lock();
+    gc_state_wait(GC_NONE);
+    gc_state_unlock();
     WITH_GC_AT_SAFEPOINTS_ONLY() {
         result = funcall0(function);
         block_blockable_signals(0, 0);
@@ -402,8 +403,6 @@ new_thread_trampoline(struct thread *th)
     unlink_thread(th);
     lock_ret = pthread_mutex_unlock(&all_threads_lock);
     gc_assert(lock_ret == 0);
-    pthread_mutex_unlock(thread_qrl(th));
-    set_thread_state(th,STATE_DEAD);
 #else
     result = funcall0(function);
 
@@ -423,9 +422,11 @@ new_thread_trampoline(struct thread *th)
 #endif
 
     if(th->tls_cookie>=0) arch_os_thread_cleanup(th);
+#ifndef LISP_FEATURE_SB_SAFEPOINT
     os_sem_destroy(th->state_sem);
     os_sem_destroy(th->state_not_running_sem);
     os_sem_destroy(th->state_not_stopped_sem);
+#endif
 
 #if defined(LISP_FEATURE_WIN32)
     free((os_vm_address_t)th->interrupt_data);
@@ -574,17 +575,16 @@ create_thread_struct(lispobj initial_function) {
      * separately */
     th->os_attr=malloc(sizeof(pthread_attr_t));
     th->nonpointer_data = nonpointer_data;
+# ifndef LISP_FEATURE_SB_SAFEPOINT
     th->state_sem=&nonpointer_data->state_sem;
     th->state_not_running_sem=&nonpointer_data->state_not_running_sem;
     th->state_not_stopped_sem=&nonpointer_data->state_not_stopped_sem;
-    th->state_not_running_waitcount = 0;
-    th->state_not_stopped_waitcount = 0;
     os_sem_init(th->state_sem, 1);
     os_sem_init(th->state_not_running_sem, 0);
     os_sem_init(th->state_not_stopped_sem, 0);
-# ifdef LISP_FEATURE_SB_SAFEPOINT
-    pthread_mutex_init(thread_qrl(th), NULL);
 # endif
+    th->state_not_running_waitcount = 0;
+    th->state_not_stopped_waitcount = 0;
 #endif
     th->state=STATE_RUNNING;
 #ifdef LISP_FEATURE_STACK_GROWS_DOWNWARD_NOT_UPWARD

src/runtime/thread.h
@@ -102,41 +102,12 @@ union per_thread_data {
  */
 struct nonpointer_thread_data
 {
-#ifdef LISP_FEATURE_SB_THREAD
+#if defined(LISP_FEATURE_SB_THREAD) && !defined(LISP_FEATURE_SB_SAFEPOINT)
     os_sem_t state_sem;
     os_sem_t state_not_running_sem;
     os_sem_t state_not_stopped_sem;
-# ifdef LISP_FEATURE_SB_SAFEPOINT
-   /* For safepoint-based builds, together with thread's
-    * csp_around_foreign_call pointer target, thread_qrl(thread) makes
-    * `quickly revokable lock'. Unlike most mutexes, this one is
-    * normally locked; by convention, other thread may read and use the
-    * thread's FFI-CSP location _either_ when the former holds the
-    * lock(mutex) _or_ when page permissions for FFI-CSP location were
-    * set to read-only.
-    *
-    * Combined semantic of QRL is not the same as the semantic of mutex
-    * returned by this function; rather, the mutex, when released by the
-    * owning thread, provides an edge-triggered notification of QRL
-    * release, which is represented by writing non-null
-    * csp_around_foreign_call.
-    *
-    * When owner thread is `in Lisp' (i.e. a heap mutator), its FFI-CSP
-    * contains null, otherwise it points to the top of C stack that
-    * should be preserved by GENCGC. If another thread needs to wait for
-    * mutator state change with `in Lisp => in C' direction, it disables
-    * FFI-CSP overwrite using page protection, and takes the mutex
-    * returned by thread_qrl(). Page fault handler normally ends up in a
-    * routine releasing this mutex and waiting for some appropriate
-    * event to take it back.
-    *
-    * This way, each thread may modify its own FFI-CSP content freely
-    * without memory barriers (paying with exception handling overhead
-    * whenever a contention happens). */
-    pthread_mutex_t qrl_lock;
-# endif
 #else
-    /* An unused field follows, to ensure that the struct in non-empty
+    /* An unused field follows, to ensure that the struct is non-empty
      * for non-GCC compilers. */
     int unused;
 #endif
@@ -382,8 +353,6 @@ void wake_thread_win32(struct thread *thread);
 int wake_thread_posix(os_thread_t os_thread);
 #  endif
 # endif
-
-#define thread_qrl(th) (&(th)->nonpointer_data->qrl_lock)
 
 static inline
 void push_gcing_safety(struct gcing_safety *into)

richardwesthaver/cl

commit b9c56834c6d40690fa93bba50c8534bd0e3ea5c1

Export *IN-WITHOUT-GCING* from SB!KERNEL and fix a C runtime bug.
This has been subtly broken ever since 8ede17a6b7 which changed the
static symbol from SB!IMPL to SB!VM without changing any of the places
where the symbol appeared in Lisp. So all the code compiled fine,
but the C constant in static-symbols.h was a totally different symbol.

package-data-list.lisp-expr
@@ -1596,6 +1596,7 @@ is a good idea, but see SB-SYS re. blurring of boundaries."
                "*EVAL-CALLS*"
                "*GC-INHIBIT*" "*GC-PENDING*"
                #!+sb-thread "*STOP-FOR-GC-PENDING*"
+               "*IN-WITHOUT-GCING*"
                "*UNIVERSAL-TYPE*"
                "*UNIVERSAL-FUN-TYPE*" "*UNPARSE-FUN-TYPE-SIMPLIFY*"
                "*WILD-TYPE*" "WORD-LOGICAL-AND" "WORD-LOGICAL-ANDC1"

src/code/globals.lisp
@@ -16,7 +16,7 @@
                   sb!debug:*stack-top-hint*
                   *handler-clusters*
                   *restart-clusters*
-                  *in-without-gcing* *gc-inhibit* *gc-pending*
+                  *gc-inhibit* *gc-pending*
                   #!+sb-thread *stop-for-gc-pending*
                   #!+sb-dynamic-core sb!vm::*required-runtime-c-symbols*
                   *posix-argv*))

src/compiler/generic/parms.lisp
@@ -171,7 +171,7 @@
     sb!unix::*interrupt-pending*
     #!+sb-thruption sb!unix::*thruption-pending*
     #!+sb-thruption sb!impl::*restart-clusters*
-    sb!vm::*in-without-gcing*
+    *in-without-gcing*
     *gc-inhibit*
     *gc-pending*
     #!-sb-thread