sbcl/sbcl

commit f8c67b2deb9f16db8798f41efe5bf2a93ec1a4b9

Optimize LAYOUT-OF for #+immobile-space, part 1 of 3
Store a layout pointer in every function header word

src/code/target-misc.lisp
@@ -99,7 +99,9 @@
                         by sb!vm:n-word-bytes
               do (setf (sap-ref-lispobj sap ofs) (%closure-index-ref closure i)))
         (setf (closure-header-word copy) ; Update the header
-              (logior (closure-header-word copy) +closure-header-namedp+)))
+              (logior #!+immobile-space (ash sb!vm:function-layout 32)
+                      +closure-header-namedp+
+                      (closure-header-word copy))))
       copy))
 
   ;; Rename a closure. Doing so changes its identity unless it was already named.

package-data-list.lisp-expr
@@ -3036,6 +3036,7 @@ structure representations"
                "SIMPLE-FUN-WIDETAG"
                "SIMPLE-FUN-NAME-SLOT"
                "FUN-POINTER-LOWTAG"
+               "FUNCTION-LAYOUT"
                "SIMPLE-FUN-SELF-SLOT"
                "SIMPLE-FUN-TYPE-SLOT"
                "GENCGC-CARD-BYTES"

src/compiler/generic/genesis.lisp
@@ -1311,6 +1311,7 @@ core and return a descriptor to it."
       (setf *layout-layout* (chill-layout 'layout t-layout s-o-layout))
       (dolist (layout (list t-layout s-o-layout *layout-layout*))
         (set-instance-layout layout *layout-layout*))
+      (chill-layout 'function t-layout)
       (chill-layout 'package t-layout s-o-layout))))
 
 ;;;; interning symbols in the cold image

src/compiler/x86-64/alloc.lisp
@@ -293,6 +293,9 @@
                   fun-pointer-lowtag)
       (storew* (logior (ash (1- size) n-widetag-bits) closure-widetag)
                result 0 fun-pointer-lowtag (not stack-allocate-p)))
+    #!+immobile-space ; store layout in header word
+    (inst mov (make-ea-for-object-slot-half result 1/2 fun-pointer-lowtag)
+          function-layout)
     (loadw temp function closure-fun-slot fun-pointer-lowtag)
     (storew temp result closure-fun-slot fun-pointer-lowtag))))
 

src/compiler/x86-64/insts.lisp
@@ -2594,9 +2594,19 @@
                                               (- n-widetag-bits
                                                  word-shift)))))))
 
+#!+immobile-space
+(progn
+  (defconstant sb!vm:function-layout ; kludge
+    (logior (+ immobile-space-start (* 3 256)) instance-pointer-lowtag))
+  #-sb-xc-host
+  (aver (eql sb!vm:function-layout
+             (get-lisp-obj-address (find-layout 'function)))))
+
 (define-instruction simple-fun-header-word (segment)
   (:emitter
-   (emit-header-data segment simple-fun-widetag)))
+   (emit-header-data segment
+                     (logior #!+immobile-space (ash sb!vm:function-layout 32)
+                             simple-fun-widetag))))
 
 ;;;; Instructions required to do floating point operations using SSE
 

src/compiler/x86-64/system.lisp
@@ -97,9 +97,10 @@
   (:results (res :scs (unsigned-reg)))
   (:result-types positive-fixnum)
   (:generator 6
-    (loadw res x 0 fun-pointer-lowtag)
-    (inst shr res n-widetag-bits)
-    (inst and res short-header-max-words)))
+    (let ((res (reg-in-size res :dword)))
+      (inst mov res (make-ea-for-object-slot-half x 0 fun-pointer-lowtag))
+      (inst shr res n-widetag-bits)
+      (inst and res short-header-max-words))))
 
 (define-vop (set-header-data)
   (:translate set-header-data)

src/runtime/gc-common.c
@@ -385,7 +385,8 @@ static lispobj
 trans_fun_header(lispobj object)
 {
     struct simple_fun *fheader = (struct simple_fun *) native_pointer(object);
-    uword_t offset = HeaderValue(fheader->header) * N_WORD_BYTES;
+    uword_t offset =
+        (HeaderValue(fheader->header) & FUN_HEADER_NWORDS_MASK) * N_WORD_BYTES;
 
     /* Transport the whole code object */
     struct code *code = (struct code *) ((uword_t) fheader - offset);

src/runtime/marknsweepgc.c
@@ -676,7 +676,7 @@ scavenge_immobile_roots(generation_index_t min_gen, generation_index_t max_gen)
 #define LAYOUT_SIZE (sizeof (struct layout)/N_WORD_BYTES)
 #define LAYOUT_ALIGN 256 /*bytes*/
 #define LAYOUT_OF_LAYOUT  ((IMMOBILE_SPACE_START+2*LAYOUT_ALIGN)|INSTANCE_POINTER_LOWTAG)
-#define LAYOUT_OF_PACKAGE ((IMMOBILE_SPACE_START+3*LAYOUT_ALIGN)|INSTANCE_POINTER_LOWTAG)
+#define LAYOUT_OF_PACKAGE ((IMMOBILE_SPACE_START+4*LAYOUT_ALIGN)|INSTANCE_POINTER_LOWTAG)
 
 // As long as Lisp doesn't have any native allocators (vops and whatnot)
 // it doesn't need to access these values.

richardwesthaver/cl

commit 6f4f6ae39acad010e9ece201d98287b13412c7fc

Fix CODE-N-UNBOXED-DATA-WORDS. Broken by change f8c67b2deb

src/code/target-misc.lisp
@@ -246,10 +246,12 @@
   (let ((f (%code-entry-point code-obj 0)))
     (or (and f
              (let ((from (code-header-words code-obj))
-                   (to (ash (with-pinned-objects (f)
+                   ;; Ignore the layout pointer (if present) in the upper bits
+                   ;; of the function header.
+                   (to (ldb (byte 24 sb!vm:n-widetag-bits)
+                            (with-pinned-objects (f)
                               (sap-ref-word (int-sap (get-lisp-obj-address f))
-                                            (- sb!vm:fun-pointer-lowtag)))
-                            (- sb!vm:n-widetag-bits))))
+                                            (- sb!vm:fun-pointer-lowtag))))))
                (and (< from to) (- to from))))
         0)))
 

tests/interface.pure.lisp
@@ -186,6 +186,13 @@
                     (the (complex single-float) y)))
                :stream (make-broadcast-stream)))
 
+;;; Data in the high bits of a fun header caused CODE-N-UNBOXED-DATA-WORDS
+;;; to return a ridiculously huge value.
+(with-test (:name (disassemble :unboxed-data))
+  (assert (< (sb-kernel:code-n-unboxed-data-words
+              (sb-kernel:fun-code-header #'expt))
+             100))) ; The exact value is irrelevant.
+
 #+x86-64
 ;; The labeler for LEA would choke on an illegal encoding
 ;; instead of showing what it illegally encodes, such as LEA RAX, RSP