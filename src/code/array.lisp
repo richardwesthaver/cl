sbcl/sbcl

commit a035448fe739b36600a13574dfa66d7fb7e23607

Put stubs into warm build phase adjacent to setf-funs
Same theory: "our" code does not need them, but ordinary code might
if executed by the interpreter.

src/code/stubs.lisp
@@ -11,7 +11,7 @@
 ;;;; provided with absolutely no warranty. See the COPYING and CREDITS
 ;;;; files for more information.
 
-(in-package "SB!IMPL")
+(in-package "SB-IMPL")
 
 (macrolet ((def (name &optional (args '(x)))
              `(defun ,name ,args
@@ -25,9 +25,9 @@
   (def binding-stack-pointer-sap  ())
   ;; x86 uses a plain old inline function for 'dynamic_space_free_pointer'
   ;; so there's no stub function for DYNAMIC-SPACE-FREE-POINTER.
-  #!-(or x86 x86-64) (def dynamic-space-free-pointer ())
+  #-(or x86 x86-64) (def dynamic-space-free-pointer ())
   (def control-stack-pointer-sap ())
-  (def sb!c:safe-fdefn-fun)
+  (def sb-c:safe-fdefn-fun)
   (def fun-subtype)
   (def simple-fun-p)
   (def %simple-fun-arglist)
@@ -37,15 +37,13 @@
   (def closurep)
   (def %closure-fun)
   (def %closure-index-ref (closure index))
-  (def sb!c::vector-length)
+  (def sb-c::vector-length)
   (def allocate-vector (type length words))
   (def make-array-header (type rank))
   (def code-instructions)
   (def code-header-ref (code-obj index))
   (def code-header-set (code-obj index new))
   (def %vector-raw-bits (object offset))
-  (def make-single-float)
-  (def make-double-float (hi lo))
   (def single-float-bits)
   (def double-float-high-bits)
   (def double-float-low-bits)
@@ -54,6 +52,6 @@
   (def %caller-pc ())
   (def %code-code-size)
   (def %code-debug-info)
-  #!+(or x86 immobile-space) (def sb!vm::%code-fixups)
+  #+(or x86 immobile-space) (def sb-vm::%code-fixups)
   (def %funcallable-instance-layout)
   (def %set-funcallable-instance-layout (x new-value)))

build-order.lisp-expr
@@ -279,8 +279,6 @@
  ("src/code/time"  :not-host)
  ("src/code/final" :not-host)
 
- ("src/code/stubs" :not-host)
-
  ("src/code/exhaust" :not-host)
 
  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;

src/code/kernel.lisp
@@ -108,3 +108,8 @@
 
 (declaim (inline lowtag-of))
 (defun lowtag-of (x) (logand (get-lisp-obj-address x) sb!vm:lowtag-mask))
+
+;;; Unlike most other "Stub functions" that never called called except
+;;; by the interpreter, these two do get called, by MAKE-UNPORTABLE-FLOAT
+(defun make-single-float (x) (make-single-float x))
+(defun make-double-float (hi lo) (make-double-float hi lo))

src/cold/warm.lisp
@@ -121,6 +121,7 @@
                 "SRC;PCL;PRECOM2"))
       (other-srcs
               '("SRC;CODE;SETF-FUNS"
+                "SRC;CODE;STUBS"
                 ;; miscellaneous functionality which depends on CLOS
                 "SRC;CODE;LATE-CONDITION"
 

richardwesthaver/cl

commit 45aeb55c2b198cd6a0f7be45d33b5db596023480

Fix ARM build, broken by a035448fe7

src/code/array.lisp
@@ -1796,3 +1796,7 @@ function to be removed without further warning."
                                   :element-type (array-element-type array))
            (array-dimensions array)))
   array)
+
+;;; Why is this needed for ARM and not x86 ???
+(defun allocate-vector (type length words)
+  (allocate-vector type length words))

src/code/stubs.lisp
@@ -38,7 +38,6 @@
   (def %closure-fun)
   (def %closure-index-ref (closure index))
   (def sb-c::vector-length)
-  (def allocate-vector (type length words))
   (def make-array-header (type rank))
   (def code-instructions)
   (def code-header-ref (code-obj index))