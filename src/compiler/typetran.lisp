sbcl/sbcl

commit a69bae9ab9aac8ae4eef524d7bdfa91df496da7a

Don't drop the other type test when checking for (and hairy constant-type).
(typep constant hairy) was transformed into
(typep constant (and (lvar-type constant) hairy))
 =>
(eql constant constant)

Fixes lp#1758962

src/compiler/typetran.lisp
@@ -96,7 +96,8 @@
             nil)
            (t
             (let ((intersect (type-intersection2 type otype)))
-              (unless intersect
+              (when (or (not intersect)
+                        (intersection-type-p intersect))
                 (tricky))
               (multiple-value-bind (constantp value)
                   (type-singleton-p intersect)
@@ -781,9 +782,10 @@
   (let ((ctype (careful-specifier-type type)))
     (if ctype
         (or
-         (multiple-value-bind (constantp value) (type-singleton-p ctype)
-           (and constantp
-                `(eql ,object ',value)))
+         (and (not (intersection-type-p type))
+              (multiple-value-bind (constantp value) (type-singleton-p ctype)
+                (and constantp
+                     `(eql ,object ',value))))
          (let ((pred (cdr (assoc ctype *backend-type-predicates*
                                  :test #'type=))))
            (when pred `(,pred ,object)))

tests/compiler-2.pure.lisp
@@ -1240,3 +1240,10 @@
               (t c))
             -1))
     ((-1) t)))
+
+(with-test (:name :typep-singleton-intersect-types)
+  (checked-compile-and-assert
+      ()
+      `(lambda ()
+         (keywordp t))
+    (() nil)))

richardwesthaver/cl

commit 6220e38f5935e29fa2e449b40daf8a073a203762

Fix typo introduced in a69bae9ab9aac8ae4eef524d7bdfa91df496da7a

src/compiler/typetran.lisp
@@ -782,7 +782,7 @@
   (let ((ctype (careful-specifier-type type)))
     (if ctype
         (or
-         (and (not (intersection-type-p type))
+         (and (not (intersection-type-p ctype))
               (multiple-value-bind (constantp value) (type-singleton-p ctype)
                 (and constantp
                      `(eql ,object ',value))))