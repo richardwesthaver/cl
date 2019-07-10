sbcl/sbcl

commit 99d9560c45114e8a366fa0fa496dbe402954091c

Make SXHASH foldable in the cross-compiler in certain cases
Just enough to get rid of circumlocution with infinities now that we have
portable representations.
Delete the CONSTANT-LVAR-P test in the transforms for symbol and string.

src/cold/set-up-cold-packages.lisp
@@ -191,6 +191,8 @@
     "ZEROP" "ABS" "SIGNUM" "FLOAT-SIGN"
     "CEILING" "FLOOR" "ROUND" "TRUNCATE" "MOD" "REM"
     ;;
+    "SXHASH" ; must package-qualify if you mean CL:SXHASH
+    ;;
     "BYTE" "BYTE-POSITION" "BYTE-SIZE"
     "DPB" "LDB" "LDB-TEST"
     "DEPOSIT-FIELD" "MASK-FIELD"))

src/compiler/sxhash.lisp
@@ -97,15 +97,17 @@
 
 ;;; SXHASH of FLOAT values is defined directly in terms of DEFTRANSFORM in
 ;;; order to avoid boxing.
-(deftransform sxhash ((x) (single-float))
+(defglobal +sxhash-single-float-expr+
   `(let ((bits (logand (single-float-bits x) ,(1- (ash 1 32)))))
      (logxor 66194023
              (sxhash (the fixnum
                           (logand sb-xc:most-positive-fixnum
                                   (logxor bits
                                           (ash bits -7))))))))
+(deftransform sxhash ((x) (single-float)) '#.+sxhash-single-float-expr+)
+
 #-64-bit
-(deftransform sxhash ((x) (double-float))
+(defglobal +sxhash-double-float-expr+
   `(let* ((hi (logand (double-float-high-bits x) ,(1- (ash 1 32))))
           (lo (double-float-low-bits x))
           (hilo (logxor hi lo)))
@@ -117,14 +119,15 @@
 
 ;;; SXHASH of FIXNUM values is defined as a DEFTRANSFORM because it's so
 ;;; simple.
-(deftransform sxhash ((x) (fixnum))
+(defglobal +sxhash-fixnum-expr+
   (let ((c (logand 1193941380939624010 sb-xc:most-positive-fixnum)))
     ;; shift by -1 to get sign bit into hash
     `(logand (logxor (ash x 4) (ash x -1) ,c) sb-xc:most-positive-fixnum)))
+(deftransform sxhash ((x) (fixnum)) '#.+sxhash-fixnum-expr+)
 
 ;;; Treat double-float essentially the same as a fixnum if words are 64 bits.
 #+64-bit
-(deftransform sxhash ((x) (double-float))
+(defglobal +sxhash-double-float-expr+
   ;; logical negation of magic constant ensures that 0.0d0 hashes to something
   ;; other than what the fixnum 0 hashes to (as tested in hash.impure.lisp)
   (let ((c (logandc1 1193941380939624010 sb-xc:most-positive-fixnum)))
@@ -134,6 +137,8 @@
        ;; This would need to shift more if N-FIXNUM-TAG-BITS > 1,
        ;; but seriously, who changes that parameter?
        (logand (logxor (ash x 4) (ash x -2) ,c) sb-xc:most-positive-fixnum))))
+
+(deftransform sxhash ((x) (double-float)) '#.+sxhash-double-float-expr+)
 
 ;;; SXHASH of SIMPLE-BIT-VECTOR values is defined as a DEFTRANSFORM
 ;;; because it is endian-dependent.
@@ -184,25 +189,15 @@
 ;;; strings, and of symbols whose names are known at compile time;
 ;;; except that since SXHASH on the cross-compilation host is not in
 ;;; general compatible with SXHASH on the target SBCL, we can't so
-;;; easily do this optimization in the cross-compiler.
-;;; To play it safe, we'll assert that SBCL itself would not benefit
-;;; from this optimization.
-(macrolet ((compiler-sxhash (x)
-             #+sb-xc-host `(error "Compiler wanted to eval (SXHASH '~S)" (lvar-value ,x))
-             #-sb-xc-host `(sxhash (lvar-value ,x))))
+;;; easily do this optimization in the cross-compiler - the SB-XC:SXHASH
+;;; operation will catch any attempt to call it on strings.
 (deftransform sxhash ((x) (string))
-  (cond ((constant-lvar-p x) (compiler-sxhash x))
-        ((csubtypep (lvar-type x) (specifier-type 'simple-string))
+  (cond ((csubtypep (lvar-type x) (specifier-type 'simple-string))
          '(%sxhash-simple-string x))
         (t
          '(%sxhash-string x))))
 (deftransform sxhash ((x) (symbol))
-  (cond ((csubtypep (lvar-type x) (specifier-type 'null))
-         ;; Test this before CONSTANT-LVAR-P because it does happen
-         ;; during cross-compilation, and we want to win, not lose.
-         (ash sb-vm::nil-value (- sb-vm:n-fixnum-tag-bits)))
-        ((constant-lvar-p x) (compiler-sxhash x))
-        ((csubtypep (lvar-type x) (specifier-type 'keyword))
+  (cond ((csubtypep (lvar-type x) (specifier-type 'keyword))
          ;; All interned symbols have a precomputed hash.
          ;; There's no way to ask the type system whether a symbol is known to
          ;; be interned, but we *can* test for the specific case of keywords.
@@ -220,10 +215,26 @@
             (if (= 0 result)
                 (ensure-symbol-hash x)
                 result)))))
-) ; end MACROLET
 
+;;; These transforms are somehow needed when compiling ARRAY-PSXHASH and
+;;; then never again.  Can't we define the guts of it to not need them?
 (deftransform psxhash ((x &optional depthoid) (character &optional t))
   `(char-code (char-upcase x)))
 
 (deftransform psxhash ((x &optional depthoid) (integer &optional t))
   `(sxhash x))
+
+;;; To define SB-XC:SXHASH compatibly without repeating the logic in the transforms
+;;; that define the numeric cases, we do some monkey business involving #. to paste
+;;; in the expressions that the transforms would return.
+#+sb-xc-host
+(progn
+  (defvar *sxhash-crosscheck* nil)
+  (defun sxhash (x)
+    (let ((answer (etypecase x ; croak on anything but these
+                    (null         (ash sb-vm::nil-value (- sb-vm:n-fixnum-tag-bits)))
+                    (fixnum       #.+sxhash-fixnum-expr+)
+                    (single-float #.+sxhash-single-float-expr+)
+                    (double-float #.+sxhash-double-float-expr+))))
+      (push (cons x answer) *sxhash-crosscheck*)
+      answer)))

src/code/late-globaldb.lisp
@@ -41,3 +41,10 @@
   ;; to detect possible inlining failures
   (def :compile-toplevel)
   (def :load-toplevel :execute))
+
+;;; This check is most effective when placed in the final cross-compiled file.
+;;; Parallelized build effectively skips this, but oh well.
+(loop for (object . hash) in '#.sb-c::*sxhash-crosscheck*
+      unless (= (sxhash object) hash)
+      do (error "SB-XC:SXHASH computed wrong answer for ~S. Got ~x should be ~x"
+                object hash (sxhash object)))

src/code/target-sxhash.lisp
@@ -556,12 +556,8 @@
                             ((float-infinity-p key)
                              ;; {single,double}-float infinities are EQUALP
                              (if (minusp key)
-                                 (load-time-value
-                                  (sxhash (symbol-value 'sb-ext:single-float-negative-infinity))
-                                  t)
-                                 (load-time-value
-                                  (sxhash (symbol-value 'sb-ext:single-float-positive-infinity))
-                                  t)))
+                                 (sxhash sb-ext:single-float-negative-infinity)
+                                 (sxhash sb-ext:single-float-positive-infinity)))
                             (t
                              (multiple-value-bind (q r) (floor key)
                                (if (zerop (the ,type r))

src/compiler/checkgen.lisp
@@ -353,7 +353,7 @@
         (map nil
              (lambda (entry)
                (let* ((canon-type (car entry))
-                      (bucket (mod (sxhash canon-type) (length hashtable))))
+                      (bucket (mod (cl:sxhash canon-type) (length hashtable))))
                  (push entry (svref hashtable bucket))))
              entries)
       hashtable))
@@ -400,7 +400,7 @@
   (let ((table **type-spec-interr-symbols**))
     (if (typep spec '(cons (eql or)))
         (%interr-symbol-for-union-type-spec spec)
-        (cdr (assoc spec (svref table (rem (sxhash spec) (length table)))
+        (cdr (assoc spec (svref table (rem (cl:sxhash spec) (length table)))
                     :test #'equal)))))
 #+nil ; some meta-analysis to decide what types should be in "generic/interr"
 (progn

src/compiler/fndb.lisp
@@ -1055,8 +1055,8 @@
   (foldable flushable))
 (defknown hash-table-size (hash-table) index (flushable))
 (defknown hash-table-test (hash-table) symbol (foldable flushable))
-(defknown sxhash (t) hash (#-sb-xc-host foldable flushable))
-(defknown psxhash (t &optional t) hash (#-sb-xc-host foldable flushable))
+(defknown sxhash (t) hash (foldable flushable))
+(defknown psxhash (t &optional t) hash (foldable flushable))
 (defknown hash-table-equalp (hash-table hash-table) boolean (foldable flushable))
 
 ;;;; from the "Arrays" chapter

richardwesthaver/cl

commit 426ac0da627bcbc691406fdb462fec6037465385

Restore building 64-bit target on 32-bit host
Quite likely broken since rev 99d9560c4511

src/cold/set-up-cold-packages.lisp
@@ -212,7 +212,14 @@
 ;;;   (* 50 (hash-table-rehash-threshold (make-hash-table)))
 ;;; because we are not intercepting '*.
 (defparameter *dual-personality-math-symbols*
-  '("+" "-" "*" "/" "=" "/=" "<" "<=" ">" ">=" "MIN" "MAX"))
+  '("+" "-" "*" "/" "=" "/=" "<" "<=" ">" ">=" "MIN" "MAX"
+    ;; We've gotten along quite well without an alter-ego for FIXNUM,
+    ;; but now some s-expressions mentioning the type FIXNUM are fed
+    ;; to the host for evaluation, and a type-checking host (such as we are)
+    ;; croaks if an argument exceeds the host's notion of fixnum.
+    ;; Get around it by changing those uses of fixnum to SB-XC:FIXNUM.
+    "FIXNUM"
+    ))
 
 ;;; When playing such tricky package games, it's best to have the symbols that
 ;;; are visible by default, i.e. XC-STRICT-CL:NAME, have no definition,

src/compiler/generic/early-vm.lisp
@@ -166,3 +166,5 @@
 ;;; The offset from the fault address reported to the runtime to the
 ;;; END of the global safepoint page.
 (defconstant gc-safepoint-trap-offset n-word-bytes)
+
+#+sb-xc-host (deftype sb-xc:fixnum () `(signed-byte ,n-fixnum-bits))

src/compiler/sxhash.lisp
@@ -100,10 +100,9 @@
 (defglobal +sxhash-single-float-expr+
   `(let ((bits (logand (single-float-bits x) ,(1- (ash 1 32)))))
      (logxor 66194023
-             (sxhash (the fixnum
+             (sxhash (the sb-xc:fixnum
                           (logand sb-xc:most-positive-fixnum
-                                  (logxor bits
-                                          (ash bits -7))))))))
+                                  (logxor bits (ash bits -7))))))))
 (deftransform sxhash ((x) (single-float)) '#.+sxhash-single-float-expr+)
 
 #-64-bit
@@ -216,7 +215,7 @@
   (defun sxhash (x)
     (let ((answer (etypecase x ; croak on anything but these
                     (null         (ash sb-vm:nil-value (- sb-vm:n-fixnum-tag-bits)))
-                    (fixnum       #.+sxhash-fixnum-expr+)
+                    (sb-xc:fixnum #.+sxhash-fixnum-expr+)
                     (single-float #.+sxhash-single-float-expr+)
                     (double-float #.+sxhash-double-float-expr+))))
       (push (cons x answer) *sxhash-crosscheck*)