sbcl/sbcl

commit 6efe9c10f69ea847258ff3983ec921f2ee44f735

Remove call to LEXENV-CONTAINS-LAMBDA.
At the very least, we shouldn't be checking the lexical
environment. Instead we should be thinking about the actual
environment present in the closure. But removing this line didn't
cause anything to break.

src/compiler/debug-dump.lisp
@@ -492,8 +492,6 @@
                         (null (basic-var-sets var))))
                (not (gethash tn (ir2-component-spilled-tns
                                  (component-info *component-being-compiled*))))
-               (lexenv-contains-lambda fun
-                                       (lambda-lexenv (lambda-var-home var)))
                (not (optional-leaf-p var))) ;; not always initialized
       (setq flags (logior flags compiled-debug-var-environment-live)))
     (when save-tn

richardwesthaver/cl

commit 09c6ab40ff2fe06bf7450e55d4abdaf925330d99

Add tests for regression caused by 6efe9c10f69ea847258ff3983ec921f2ee44f735.

tests/debug.impure.lisp
@@ -1051,6 +1051,30 @@
                          (f2))))))))
     (assert (= count 3))))
 
+(with-test (:name :indirect-closure-values.crash)
+  (block nil
+    (handler-bind ((error (lambda (c)
+                            (declare (ignore c))
+                            (sb-debug:map-backtrace
+                             (lambda (frame)
+                               (let ((name (sb-debug::frame-call frame))
+                                     (location (sb-debug::frame-code-location frame))
+                                     (d-fun (sb-debug::frame-debug-fun frame)))
+                                 (when (eq name 'test)
+                                   (assert (sb-debug::debug-var-info-available d-fun))
+                                   (dolist (v (sb-debug::ambiguous-debug-vars d-fun ""))
+                                     (assert (not (sb-debug::var-valid-in-frame-p v location frame))))
+                                   (return))))))))
+      (funcall
+       (compile nil
+                `(sb-int:named-lambda test ()
+                   (declare (optimize debug safety))
+                   (signal 'error)
+                   (let ((protos '()))
+                     (mapcar (lambda (x)
+                               (print x))
+                             protos))))))))
+
 (with-test (:name :non-tail-self-call-bad-variables)
   (let ((count 0))
     (block nil