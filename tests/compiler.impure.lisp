sbcl/sbcl

commit 36a379d746b9eb74ba8c5afff40dc5dcb9f4557a

0.9.11.38:         * Fix MISC.555: remove a function from *FREE-FUNS* when it has           only local calls.         * Null :SLOT-NAMES argument of MAKE-LOAD-FORM-SAVING-SLOTS           means no saved slots.

src/compiler/ir1util.lisp
@@ -1015,6 +1015,17 @@
 
   (values))
 
+(defun note-local-functional (fun)
+  (declare (type functional fun))
+  (when (and (leaf-has-source-name-p fun)
+             (eq (leaf-source-name fun) (functional-debug-name fun)))
+    (let ((name (leaf-source-name fun)))
+      (let ((defined-fun (gethash name *free-funs*)))
+        (when (and defined-fun
+                   (defined-fun-p defined-fun)
+                   (eq (defined-fun-functional defined-fun) fun))
+          (remhash name *free-funs*))))))
+
 ;;; Do stuff to delete the semantic attachments of a REF node. When
 ;;; this leaves zero or one reference, we do a type dispatch off of
 ;;; the leaf to determine if a special action is appropriate.

src/compiler/locall.lisp
@@ -274,7 +274,8 @@
 ;;; do LET conversion here.
 (defun locall-analyze-fun-1 (fun)
   (declare (type functional fun))
-  (let ((refs (leaf-refs fun)))
+  (let ((refs (leaf-refs fun))
+        (local-p t))
     (dolist (ref refs)
       (let* ((lvar (node-lvar ref))
              (dest (when lvar (lvar-dest lvar))))
@@ -286,9 +287,12 @@
                  (convert-call-if-possible ref dest)
 
                  (unless (eq (basic-combination-kind dest) :local)
-                   (reference-entry-point ref)))
+                   (reference-entry-point ref)
+                   (setq local-p nil)))
                 (t
-                 (reference-entry-point ref)))))))
+                 (reference-entry-point ref)
+                 (setq local-p nil))))))
+    (when local-p (note-local-functional fun)))
 
   (values))
 

tests/compiler.pure.lisp
@@ -2078,3 +2078,11 @@
              :bad2))
     :good))))
   (assert (eq (funcall (compile nil f)) :good)))
+
+;;; MISC.555: new reference to an already-optimized local function
+(let* ((l '(lambda (p1)
+    (declare (optimize (speed 1) (safety 2) (debug 2) (space 0)) (type keyword p1))
+    (keywordp p1)))
+       (f (compile nil l)))
+  (assert (funcall f :good))
+  (assert (nth-value 1 (ignore-errors (funcall f 42)))))

BUGS
@@ -1928,25 +1928,6 @@ WORKAROUND:
  #.SB-EXT:SINGLE/DOUBLE-FLOAT-POSITIVE-INFINITY. These tests have been
  disabled on Darwin for now.
 
-375: MISC.555
-    (compile nil '(lambda (p1)
-		    (declare (optimize (speed 1) (safety 2) (debug 2) (space 0))
-			     (type keyword p1))
-		    (keywordp p1)))
-
-  fails on hairy type check in IR2.
-
-  1. KEYWORDP is MAYBE-INLINE expanded (before TYPEP-like
-     transformation could eliminate it).
-
-  2. From the only call of KEYWORDP the type of its argument is
-     derived to be KEYWORD.
-
-  2. Type check for P1 is generated; it uses KEYWORDP to perform the
-     check, and so references the local function; from the KEYWORDP
-     argument type new CAST to KEYWORD is generated. The compiler
-     loops forever.
-
 377: Memory fault error reporting
   On those architectures where :C-STACK-IS-CONTROL-STACK is in
   *FEATURES*, we handle SIG_MEMORY_FAULT (SEGV or BUS) on an altstack,

src/pcl/env.lisp
@@ -157,14 +157,14 @@
   (error "~@<don't know how to dump ~S (default ~S method called).~@>"
          object 'make-load-form))
 
-(defun make-load-form-saving-slots (object &key slot-names environment)
+(defun make-load-form-saving-slots (object &key (slot-names nil slot-names-p) environment)
   (declare (ignore environment))
   (let ((class (class-of object)))
     (collect ((inits))
       (dolist (slot (class-slots class))
         (let ((slot-name (slot-definition-name slot)))
           (when (or (memq slot-name slot-names)
-                    (and (null slot-names)
+                    (and (not slot-names-p)
                          (eq :instance (slot-definition-allocation slot))))
             (if (slot-boundp-using-class class object slot)
                 (let ((value (slot-value-using-class class object slot)))

tests/dump.impure-cload.lisp
@@ -89,6 +89,17 @@
 (assert (eql (savable-structure-c *savable-structure*) 1))
 (assert (eql (savable-structure-d *savable-structure*) 39))
 (assert (eql (savable-structure-e *savable-structure*) 19))
+
+;;; null :SLOT-NAMES /= unsupplied
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (defclass savable-class ()
+    ((a :initform t :initarg :a)))
+  (defmethod make-load-form ((s savable-class) &optional env)
+    (make-load-form-saving-slots s :environment env :slot-names '())))
+(defparameter *savable-class*
+  #.(make-instance 'savable-class :a 3))
+(assert (not (slot-boundp *savable-class* 'a)))
+
 
 ;;; ensure that we can dump and reload specialized arrays whose element
 ;;; size is smaller than a byte (caused a few problems circa SBCL

version.lisp-expr
@@ -17,4 +17,4 @@
 ;;; checkins which aren't released. (And occasionally for internal
 ;;; versions, especially for internal versions off the main CVS
 ;;; branch, it gets hairier, e.g. "0.pre7.14.flaky4.13".)
-"0.9.11.37"
+"0.9.11.38"

richardwesthaver/cl

commit 05f7e2ad7a5cf9f7ed44ce9c1f0e2d43de6c49e6

Delete NOTE-LOCAL-FUNCTIONAL, removing a KLUDGE.
Commit 36a379d746b9eb74ba8c5afff40dc5dcb9f4557a introduced this
function to work around issues of referencing already converted local
functionals. Turns out this was only needed because the IR1 %DEFUN
translator was rewritten incorrectly when it became the
EVAL-WHEN (:COMPILE-TOPLEVEL ...) style of top level processing. (And
note that CMU CL didn't have this problem). The named lambda machinery
meant to replace the IR1 translator is a weird twisted duplicated
version of IR1-CONVERT-INLINE-EXPANSION, and handling the
DEFINED-FUNS/FREE-FUNS stuff became wrong. Fix the discrepancy,
allowing us to get rid of NOTE-LOCAL-FUNCTIONAL. We still essentially
have two duplicated code paths though. It would be nice to reunify
them again like they were originally before the IR1 translator for
%DEFUN was removed.

ALso, make sure to nuke the ir1-namespace after we finish compiling a
component. That is now another way to have dangling references to
already converted functionals.

Move ftype clearing from %COMPILER-DEFUN to named lambda processing:
THis way we still warn about self call argument type conflicting
without messing with GET-DEFINED-FUN. This also tackles 2 FIXMEs: It's
clear that line has nothing to do with block compilation and
everything to do with ftypes for redefined functions. It's also now
clear that asserting the proclaimed type is orthogonal to whether we
have a non-entry block compiled function.

The upshot of this change is that now it's easier to see that we are
actually treating local functionals correctly with block compilation
on. Before, it just seemed like we were doing an ad-hoc workaround
that might expose the same bug in NOTE-LOCAL-FUNCTIONAL with it turned
on.

Incidentally fix a test that abused named-lambda: It was only working
coincidentally because we got the logic for functional saving wrong:
It was recognizing the self call even without the recognize-self-calls
declaration.

src/compiler/ir1tran-lambda.lisp
@@ -1007,56 +1007,68 @@
     ((named-lambda)
      (let* ((name (cadr thing))
             (lambda-expression `(lambda ,@(cddr thing)))
-            (*inline-expansions* (list name 1 *inline-expansions*))
-            (simple-lexenv-p (simple-lexical-environment-p *lexenv*)))
-       ;; Discard any forward references to this function unless we
-       ;; are block compiling and the lexical environment doesn't
-       ;; contain any hair. If the lexical environment is too hairy, then we only
-       ;; install the definition during the processing of this NAMED-LAMBDA,
-       ;; ensuring that the function cannot be called outside of the correct
-       ;; environment.
-       (unless (and (block-compile *compilation*) simple-lexenv-p)
-         (remhash name (free-funs *ir1-namespace*)))
+            (*inline-expansions* (list name 1 *inline-expansions*)))
        (if (and name (legal-fun-name-p name))
-           (let ((defined-fun-res (get-defined-fun name (second lambda-expression)))
-                 (res (ir1-convert-lambda lambda-expression
-                                          :maybe-add-debug-catch t
-                                          :source-name name))
-                 (info (info :function :info name)))
-             (setf (functional-inlinep res) (info :function :inlinep name)
-                   (defined-fun-same-block-p defined-fun-res) t)
-             (when (has-toplevelness-decl lambda-expression)
-               (setf (functional-top-level-defun-p res) t))
-             ;; FIXME: Should non-entry block compiled defuns have
-             ;; this propagate?
-             (unless (and info
-                          (ir1-attributep (fun-info-attributes info) fixed-args))
-               (assert-global-function-definition-type name res))
-             ;; If in a simple environment, then we can allow
-             ;; backward references to this function from following
-             ;; top-level forms.
-             (when simple-lexenv-p
-               (setf (defined-fun-functional defined-fun-res) res))
-             (unless (or
-                      (eq (defined-fun-inlinep defined-fun-res) 'notinline)
-                      ;; Don't treat recursive stubs like CAR as self-calls
-                      ;; Maybe just use the fact that it is a known function?
-                      ;; Though a known function may be used
-                      ;; because of some other attributues but
-                      ;; still wants to get optimized self calls
-                      (and info
-                           (or (fun-info-templates info)
-                               (fun-info-transforms info)
-                               (fun-info-ltn-annotate info)
-                               (fun-info-ir2-convert info)
-                               (fun-info-optimizer info))))
-               (if (block-compile *compilation*)
-                   (substitute-leaf res defined-fun-res)
-                   (substitute-leaf-if
-                    (lambda (ref)
-                      (policy ref (> recognize-self-calls 0)))
-                    res defined-fun-res)))
-             res)
+           (let ((simple-lexenv-p (simple-lexical-environment-p *lexenv*)))
+             ;; If not in a simple environment, then discard any
+             ;; forward references to this function. If the lexical
+             ;; environment is too hairy, then we only install the
+             ;; definition during the processing of this NAMED-LAMBDA,
+             ;; ensuring that the function cannot be called outside of
+             ;; the correct environment. If the function is globally
+             ;; NOTINLINE, then that inhibits even local substitution.
+             (unless simple-lexenv-p
+               (remhash name (free-funs *ir1-namespace*)))
+             (let ((defined-fun-res (get-defined-fun name)))
+               ;;
+               ;; If there is a type from a previous definition, blast it, since it is
+               ;; obsolete.
+               (when (neq :declared (leaf-where-from defined-fun-res))
+                 (setf (leaf-type defined-fun-res)
+                       ;; Use the type from the lambda list so that self
+                       ;; calls warn about mismatched args.
+                       (let ((lambda-list (second lambda-expression)))
+                         (or (and lambda-list
+                                  (ignore-errors
+                                   (ftype-from-lambda-list lambda-list)))
+                             (specifier-type 'function)))))
+               (let ((res (ir1-convert-lambda lambda-expression
+                                              :maybe-add-debug-catch t
+                                              :source-name name))
+                     (info (info :function :info name)))
+                 (setf (functional-inlinep res) (info :function :inlinep name)
+                       (defined-fun-same-block-p defined-fun-res) t)
+                 (when (has-toplevelness-decl lambda-expression)
+                   (setf (functional-top-level-defun-p res) t))
+                 (unless (and info
+                              (ir1-attributep (fun-info-attributes info) fixed-args))
+                   (assert-global-function-definition-type name res))
+                 (unless (or
+                          (eq (defined-fun-inlinep defined-fun-res) 'notinline)
+                          ;; Don't treat recursive stubs like CAR as self-calls
+                          ;; Maybe just use the fact that it is a known function?
+                          ;; Though a known function may be used
+                          ;; because of some other attributues but
+                          ;; still wants to get optimized self calls
+                          (and info
+                               (or (fun-info-templates info)
+                                   (fun-info-transforms info)
+                                   (fun-info-ltn-annotate info)
+                                   (fun-info-ir2-convert info)
+                                   (fun-info-optimizer info))))
+                   (if (block-compile *compilation*)
+                       (progn
+                         (substitute-leaf res defined-fun-res)
+                         ;; If in a simple environment, then we can allow
+                         ;; backward references to this function from
+                         ;; following top-level forms.
+                         (when simple-lexenv-p
+                           (setf (defined-fun-functional defined-fun-res) res)))
+                       (substitute-leaf-if
+                        (lambda (ref)
+                          (policy ref (> recognize-self-calls 0)))
+                        res defined-fun-res)))
+                 res)))
            (ir1-convert-lambda lambda-expression
                                :maybe-add-debug-catch t
                                :debug-name
@@ -1193,7 +1205,7 @@
 ;;; Get a DEFINED-FUN object for a function we are about to define. If
 ;;; the function has been forward referenced, then substitute for the
 ;;; previous references.
-(defun get-defined-fun (name &optional (lambda-list nil lp))
+(defun get-defined-fun (name)
   (proclaim-as-fun-name name)
   (let ((found (find-free-fun name "shouldn't happen! (defined-fun)"))
         (free-funs (free-funs *ir1-namespace*)))
@@ -1213,19 +1225,14 @@
                         :where-from (if (eq where-from :declared)
                                         :declared
                                         :defined-here)
-                        :type (if (eq :declared where-from)
-                                  (leaf-type found)
-                                  (or (and lp
-                                           (ignore-errors
-                                            (ftype-from-lambda-list lambda-list)))
-                                      (specifier-type 'function))))))
+                        :type (leaf-type found))))
              (substitute-leaf res found)
              (setf (gethash name free-funs) res)))
           ;; If FREE-FUNS has a previously converted definition
           ;; for this name, then blow it away and try again.
           ((defined-fun-functional found)
            (remhash name free-funs)
-           (get-defined-fun name lambda-list))
+           (get-defined-fun name))
           (t found))))
 
 ;;; Check a new global function definition for consistency with
@@ -1406,18 +1413,11 @@ is potentially harmful to any already-compiled callers using (SAFETY 0)."
 ;;; The inline lambda will be NIL for a structure accessor, predicate, or copier
 ;;; since those can always be reconstructed from a defstruct description.
 (defun %compiler-defun (name compile-toplevel inline-lambda extra-info)
-  (let ((defined-fun nil)) ; will be set below if we're in the compiler
-    (cond (compile-toplevel
+  (cond (compile-toplevel
+         (let ((defined-fun nil))
            (with-single-package-locked-error
                (:symbol name "defining ~S as a function")
-             (setf defined-fun
-                   ;; Try to pass the lambda-list to GET-DEFINED-FUN if we can.
-                   (if (atom inline-lambda)
-                       (get-defined-fun name)
-                       (get-defined-fun
-                        name (ecase (car inline-lambda)
-                               (lambda-with-lexenv (third inline-lambda))
-                               (lambda (second inline-lambda)))))))
+             (setf defined-fun (get-defined-fun name)))
            (when (boundp '*lexenv*)
              (aver (producing-fasl-file))
              (if (member name (fun-names-in-this-file *compilation*) :test #'equal)
@@ -1425,22 +1425,12 @@ is potentially harmful to any already-compiled callers using (SAFETY 0)."
                  (push name (fun-names-in-this-file *compilation*))))
            ;; I don't know why this is guarded by (WHEN compile-toplevel),
            ;; because regular old %DEFUN is going to call this anyway.
-           (%set-inline-expansion name defined-fun inline-lambda extra-info))
-          ((boundp 'sb-fasl::*current-fasl-group*)
-           (if (member name (sb-fasl::fasl-group-fun-names sb-fasl::*current-fasl-group*) :test #'equal)
-               (warn 'duplicate-definition :name name)
-               (push name (sb-fasl::fasl-group-fun-names sb-fasl::*current-fasl-group*)))))
+           (%set-inline-expansion name defined-fun inline-lambda extra-info)))
+        ((boundp 'sb-fasl::*current-fasl-group*)
+         (if (member name (sb-fasl::fasl-group-fun-names sb-fasl::*current-fasl-group*) :test #'equal)
+             (warn 'duplicate-definition :name name)
+             (push name (sb-fasl::fasl-group-fun-names sb-fasl::*current-fasl-group*)))))
 
-    (become-defined-fun-name name)
-
-    ;;
-    ;; If there is a type from a previous definition, blast it, since it is
-    ;; obsolete.
-    (when (and defined-fun (neq :declared (leaf-where-from defined-fun)))
-      (setf (leaf-type defined-fun)
-            ;; FIXME: If this is a block compilation thing, shouldn't
-            ;; we be setting the type to the full derived type for the
-            ;; definition, instead of this most general function type?
-            (specifier-type 'function))))
+  (become-defined-fun-name name)
 
   (values))

src/compiler/ir1util.lisp
@@ -1360,24 +1360,6 @@
           (setf (ctran-block ctran) new-block))
         new-block))))
 
-;;; This is called by locall-analyze-fun-1 after it convers a call to
-;;; FUN into a local call.
-;;; Presumably, the function can be no longer reused by new calls to
-;;; FUN, so the whole thing has to be removed from (FREE-FUN *IR1-NAMESPACE*).
-(defun note-local-functional (fun &aux (free-funs (free-funs *ir1-namespace*)))
-  (declare (type functional fun))
-  (when (and (leaf-has-source-name-p fun)
-             (eq (leaf-source-name fun) (functional-debug-name fun)))
-    (let* ((name (leaf-source-name fun))
-           (defined-fun (gethash name free-funs)))
-      (when (and (defined-fun-p defined-fun)
-                 ;; KLUDGE: We must not blow away the free-fun entry
-                 ;; while block compiling. It would be better to get
-                 ;; rid of this function entirely and untangle this
-                 ;; mess, since this is really a workaround.
-                 (not (eq (block-compile *compilation*) t)))
-        (remhash name free-funs)))))
-
 
 ;;;; deleting stuff
 

src/compiler/locall.lisp
@@ -379,8 +379,7 @@
 ;;; do LET conversion here.
 (defun locall-analyze-fun-1 (fun)
   (declare (type functional fun))
-  (let ((refs (leaf-refs fun))
-        (local-p t))
+  (let ((refs (leaf-refs fun)))
     (dolist (ref refs)
       (let* ((lvar (node-lvar ref))
              (dest (when lvar (lvar-dest lvar))))
@@ -394,12 +393,9 @@
                  (when (eq (functional-kind fun) :deleted)
                    (return-from locall-analyze-fun-1))
                  (unless (eq (basic-combination-kind dest) :local)
-                   (reference-entry-point ref)
-                   (setq local-p nil)))
+                   (reference-entry-point ref)))
                 (t
-                 (reference-entry-point ref)
-                 (setq local-p nil))))))
-    (when local-p (note-local-functional fun)))
+                 (reference-entry-point ref)))))))
 
   (values))
 

src/compiler/main.lisp
@@ -829,6 +829,21 @@ necessary, since type inference may take arbitrarily long to converge.")
     ;; Its refs will be scanned redundantly, which is harmless.
     (blast (eql-constants ns)))
   (values))
+
+;;; Clear the global hash tables held in IR1-NAMESPACE.
+(defun clear-ir1-namespace ()
+  (when (boundp '*ir1-namespace*)
+    (let ((ir1-namespace *ir1-namespace*))
+      (clrhash (free-funs ir1-namespace))
+      (clrhash (free-vars ir1-namespace))
+      ;; FIXME: It would make sense to clear these tables as well, but
+      ;; ARM64 relies on the constant for NIL to stay around in order to
+      ;; assign a wired TN to it, so we let people share it. Investigate a
+      ;; proper fix.
+      #+(or)
+      (progn
+        (clrhash (eql-constants ir1-namespace))
+        (clrhash (similar-constants ir1-namespace))))))
 
 ;;;; trace output
 
@@ -1593,7 +1608,9 @@ necessary, since type inference may take arbitrarily long to converge.")
           (compile-load-time-value-lambda lambdas)
           (compile-toplevel-lambdas lambdas top-level-closure)))
 
-    (mapc #'clear-ir1-info components))
+    (dolist (component components)
+      (clear-ir1-info component))
+    (clear-ir1-namespace))
   (values))
 
 ;;; Actually compile any stuff that has been queued up for block

tests/compiler.impure.lisp
@@ -3137,3 +3137,13 @@
                   (sb-int:info :function :type 'non-top-level-type-derive))
                  '(function () (values (integer 1 1) &optional))))
   (assert (eql (funcall 'non-top-level-type-derive) 1)))
+
+(with-test (:name :delete-optional-dispatch-xep)
+  (ctu:file-compile
+   "(defun delete-optional-dispatch-xep (&optional x)
+      (if (= x 0)
+          10
+          (multiple-value-call #'delete-optional-dispatch-xep (1- x))))"
+   :block-compile t ; so the self call is recognized
+   :load t)
+  (assert (= (funcall 'delete-optional-dispatch-xep 3) 10)))

tests/compiler.pure.lisp
@@ -5935,15 +5935,6 @@
                    s)))
     (('(1 2) 3) 6)))
 
-(with-test (:name :delete-optional-dispatch-xep)
-  (let ((name (gensym)))
-    (checked-compile-and-assert ()
-        `(sb-int:named-lambda ,name (&optional x)
-           (if (= x 0)
-               10
-               (multiple-value-call #',name (1- x))))
-      ((3) 10))))
-
 (with-test (:name :bug-1951889)
   (checked-compile-and-assert
    (:optimize '(:debug 2))